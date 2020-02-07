ce4d630c07c1eb160e8696300beb15cfd6f3b72f
Rainyer Santiago <rainyer.santiago@gmail.com>
2020-02-06 21:31:13 -0300

    initial commit

---
+++ App.js
@@ -0,0 +1,150 @@
+import React from 'react'
+import {View, Text, ScrollView, Animated, StyleSheet} from 'react-native'
+import {createAppContainer} from 'react-navigation'
+import {createStackNavigator} from 'react-navigation-stack'
+import items from './items.json'
+
+class HomeScreen extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      scrollY: new Animated.Value(0),
+    }
+  }
+
+  getItems = () => {
+    return items.map((item, i) => {
+      return (
+        <View key={String(i)} style={styles.item}>
+          <Text>{item.title}</Text>
+        </View>
+      )
+    })
+  }
+
+  render() {
+    const {scrollY} = this.state
+
+    return (
+      <>
+        <AnimatedHeader scrollY={scrollY} />
+        <Animated.ScrollView
+          contentContainerStyle={styles.scrollViewContentStyles}
+          scrollEventThrottle={1}
+          onScroll={Animated.event(
+            [{nativeEvent: {contentOffset: {y: this.state.scrollY}}}],
+            {useNativeDriver: true},
+          )}
+        >
+          {this.getItems()}
+        </Animated.ScrollView>
+      </>
+    )
+  }
+}
+
+const AnimatedHeader = ({scrollY}) => {
+  const endingHeaderAnimation = scrollY.interpolate({
+    inputRange: [0, 100],
+    outputRange: [0, 1],
+    extrapolate: 'clamp',
+  })
+
+  const initialHeaderAnimation = scrollY.interpolate({
+    inputRange: [0, 1],
+    outputRange: [1, 0],
+    extrapolate: 'clamp',
+  })
+
+  return (
+    <>
+      <ColoredHeader opacityAnimatedValue={endingHeaderAnimation} />
+      <TransparentHeader opacityAnimatedValue={initialHeaderAnimation} />
+    </>
+  )
+}
+
+const TransparentHeader = ({opacityAnimatedValue}) => {
+  return (
+    <Animated.View
+      style={{
+        ...styles.header,
+        backgroundColor: 'transparent',
+        opacity: opacityAnimatedValue,
+      }}
+    >
+      <Text
+        style={{
+          ...styles.headerTitle,
+          color: 'white',
+        }}
+      >
+        Transparent Header
+      </Text>
+    </Animated.View>
+  )
+}
+
+const ColoredHeader = ({opacityAnimatedValue}) => {
+  return (
+    <Animated.View
+      style={{
+        ...styles.header,
+        backgroundColor: 'white',
+        opacity: opacityAnimatedValue,
+      }}
+    >
+      <Text
+        style={{
+          ...styles.headerTitle,
+          color: 'black',
+        }}
+      >
+        Colored Header
+      </Text>
+    </Animated.View>
+  )
+}
+
+const AppNavigator = createStackNavigator({
+  Home: {
+    screen: HomeScreen,
+    navigationOptions: () => {
+      return {
+        header: null,
+      }
+    },
+  },
+})
+
+const styles = StyleSheet.create({
+  scrollViewContentStyles: {
+    backgroundColor: '#836fa9',
+    paddingTop: 80,
+    paddingHorizontal: 16,
+  },
+  header: {
+    width: '100%',
+    zIndex: 200,
+    position: 'absolute',
+    top: 0,
+    height: 80,
+    justifyContent: 'center',
+    alignItems: 'center',
+    padding: 10,
+  },
+  item: {
+    marginBottom: 20,
+    backgroundColor: '#ca9b52',
+    height: 60,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  headerTitle: {
+    fontSize: 18,
+    fontWeight: '500',
+    marginTop: 10,
+  },
+})
+
+export default createAppContainer(AppNavigator)
